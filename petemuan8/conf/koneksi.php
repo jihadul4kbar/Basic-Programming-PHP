3beb869d086c6e191c029aef00cac03cda5a1641
jihadul4kbar <jihadul4kbar>
2024-06-12 15:03:12 +0800

    pertemuan 8

---
+++ petemuan8/conf/koneksi.php
@@ -0,0 +1,18 @@
+<?php
+
+// Koneksi ke dalam database
+
+$koneksi = mysqli_connect("localhost","root","","belajarphp");
+
+// Fungsi untuk menambil data dari Database
+function query($query) {
+	global $koneksi;
+	$result = mysqli_query($koneksi, $query);
+	$rows = [];
+	while ($mhs = mysqli_fetch_assoc($result)) {
+		$rows[] = $mhs;
+	}
+	return $rows;
+}
+
+?>
