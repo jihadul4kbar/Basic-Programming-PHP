5ec19db7a4f8dd00868404bdb49257c961412e15
jihadul4kbar <jihadul4kbar@gmail.com>
2025-01-02 16:15:29 +0800

    perbaikan struktur

--- pertemuan7/footer.php
+++
@@ -1,4 +0,0 @@
-
-<p style="text-align:center">Copyright @ Pemrograman Web 2020</p>
-</body>
-</html>
--- pertemuan7/index.php
+++
@@ -1,6 +0,0 @@
-
-<?php include "header.php";?>
-
-<h1>Selamat Datang di Aplikasi Akademik </h1>
-
-<?php include "footer.php";?>
